541026320874d3054e825efa349ac2a971c0ab31
viskiarditia <viskiarditia@gmail.com>
2023-03-03 14:46:50 +0700

    First Commit for PET

--- src/main/java/pageObject/PET/PETpage.java
+++ src/main/java/pageObject/PET/PETpage.java
@@ -13,38 +13,78 @@ public class PETpage {
         PageFactory.initElements(driver, this);
         this.driver = driver;
     }
-
-    @FindBy(xpath="//input[@id='user-name']")
-    private WebElement userName;
-    public void setUserName(String user){
-        userName.sendKeys(user);
-    }
-
-
-    @FindBy(xpath ="//input[@id='password']")
-    private WebElement password;
-    public void setPassword(String psd){
-        password.sendKeys(psd);
-    }
-
-
-    @FindBy(id ="login-button")
-    private WebElement btnLogin;
-    public void clickLogin(){
-        btnLogin.click();
-    }
-
-
-    @FindBy(xpath = "//h3")
-    private WebElement errorText;
-    public String verifyErrorText(){
-        return errorText.getText();
-    }
-
-
-    public boolean verifyLoginPage(){
-        userName.isDisplayed();
+    // Logo Home Page Amazon
+    @FindBy(xpath="//a[@id='nav-logo-sprites']" )
+    private WebElement logoAmazonHome;
+    public boolean setVerifyHomePage(){
+        logoAmazonHome.isDisplayed();
         return true;
+    }
+    // Menu All Hammburger button
+    @FindBy(xpath = "//a[@id='nav-hamburger-menu']")
+    private WebElement hamburgerMenuButton;
+    public void setHamburgerMenuButton(){
+        hamburgerMenuButton.click();
+    }
+    // Sub Menu Smart Home on Hamburger All menu
+    @FindBy(xpath = "//a[.='Smart Home']")
+    private WebElement hamburgerMenuItem;
+    public void setHamburgerMenuItem(){
+        hamburgerMenuItem.click();
+    }
+    // Sub Menu PET on menu Smart Home
+    @FindBy(xpath = "//a[.='Pet']")
+    private WebElement hamburgerListItem;
+    public void setHamburgerListItem(){
+        hamburgerListItem.click();
+    }
+    // The First Product
+    @FindBy(xpath = "//*[@id=\"acs-product-block-0\"]")
+    private WebElement firstProduct;
+    public void setFirstProduct(){
+        firstProduct.click();
+    }
+    // Add to Cart
+    @FindBy(xpath = "//input[@id='add-to-cart-button']")
+    private WebElement addToCart;
+    public void setAddToCart(){
+        addToCart.click();
+    }
+    // The Second Product
+    @FindBy(xpath = "//ol[@class='a-carousel']//div[contains(.,'HD 1080p Pet Camera，Dog Camera 360° Pet Monitor Indoor Cat Camera with Night Vis')]")
+    private WebElement secondProduct;
+    public void setSecondProduct(){
+        secondProduct.click();
+    }
+    // Delete Product
+    @FindBy(xpath = "//div[@class='a-section a-spacing-mini sc-list-body sc-java-remote-feature']/div[4]//span[@class='a-size-small sc-action-delete']//input[@class='a-color-link']")
+    private WebElement deleteProduct;
+    public void setDeleteProduct(){
+        deleteProduct.click();
+    }
+    // Checkout product
+    @FindBy(xpath = "//input[@name='proceedToRetailCheckout']")
+    private WebElement buttonCheckOut;
+    public void setButtonCheckOut(){
+        buttonCheckOut.click();
+    }
+    // input email or mobile phone number
+    @FindBy(xpath = "//input[@id='ap_email']")
+    private WebElement emailOrMobilePhoneNumber;
+    public void setEmailOrMobilePhoneNumber(String userName){
+       emailOrMobilePhoneNumber.sendKeys(userName);
+    }
+    // click button continue
+    @FindBy(xpath = "//input[@id='continue']")
+    private WebElement buttonContinue;
+    public void setButtonContinue(){
+        buttonContinue.click();
+    }
+    // display error message
+    @FindBy(xpath = "//span[@class='a-list-item']")
+    private WebElement errorMessage;
+    public String setErrorMessage(){
+        return errorMessage.getText();
     }
 }
 
